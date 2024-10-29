247541a689719f19ced59b4d8603d2c491fa07cd
Francesco Falconaro <francesco.falconaro@nttdata.com>
2024-10-29 15:14:14 +0100

    change columns.js

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -15,4 +15,21 @@ export default function decorate(block) {
       }
     });
   });
+  // Process columns metadata
+  const columnsMeta = section.querySelector('div.columns-metadata');
+  if (columnsMeta) {
+    const meta = readBlockConfig(columnsMeta);
+    Object.keys(meta).forEach((key) => {
+      if (key === 'style') {
+        const styles = meta.style
+          .split(',')
+          .filter((style) => style)
+          .map((style) => toClassName(style.trim()));
+        styles.forEach((style) => section.classList.add(style));
+      } else {
+        section.dataset[toCamelCase(key)] = meta[key];
+      }
+    });
+    columnsMeta.parentNode.remove();
+  }
 }
