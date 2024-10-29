4fa138f1a91cc71cf0431be6e6cb06d2efca22e0
Francesco Falconaro <francesco.falconaro@nttdata.com>
2024-10-29 15:34:30 +0100

    change columns.js

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,9 +1,3 @@
-import {
-  readBlockConfig,
-  toClassName,
-  toCamelCase,
-} from '../../scripts/aem.js';
-
 export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
@@ -21,21 +15,4 @@ export default function decorate(block) {
       }
     });
   });
-  // Process columns metadata
-  const columnsMeta = section.querySelector('div.columns-metadata');
-  if (columnsMeta) {
-    const meta = readBlockConfig(columnsMeta);
-    Object.keys(meta).forEach((key) => {
-      if (key === 'style') {
-        const styles = meta.style
-          .split(',')
-          .filter((style) => style)
-          .map((style) => toClassName(style.trim()));
-        styles.forEach((style) => section.classList.add(style));
-      } else {
-        section.dataset[toCamelCase(key)] = meta[key];
-      }
-    });
-    columnsMeta.parentNode.remove();
-  }
 }
